4425841c50fce13bb46e58dfb8ffd772ce9cc635
Aravind Raj <aravinddevaraj777@gmail.com>
2018-12-26 22:50:11 +0530

    user verify (test)
    Aravind Raj

--- routes/registration.js
+++ routes/registration.js
@@ -4,11 +4,20 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const config = require('../config/env');
 const User = require('../models/user');
+var nodemailer = require("nodemailer");
 
-
+//Fill up mail details and proceed
+let smtpTransport = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: "your mail",
+        pass: "password"
+    }
+});
 
 //Create Registration User
 router.post('/create', (req, res, next) => {
+    let rand = Math.floor((Math.random() * 1000000));
     let newUser = new User({
         name: req.body.name,
         college_id: req.body.college_id,
@@ -20,7 +29,6 @@ router.post('/create', (req, res, next) => {
         mobile_number: req.body.mobile_number,
         confirmed: false,
         activated: false,
-        activation_code: Math.floor((Math.random() * 10000)),
         type: req.body.type,
         password: req.body.password
     });
@@ -29,25 +37,63 @@ router.post('/create', (req, res, next) => {
         if (err) {
             res.json({ success: false, msg: 'Failed to register user' + err });
         } else {
-            res.json({ success: true, msg: 'User registered' });
+            res.json({ success: true, msg: 'User Activated' });            
+        }
+    });
+});
+
+router.post('/activate',function(req, res, next){
+    let host=req.get('host');
+    let id = "";
+    User.getUserByEmailId(req.body.email_id , (err, result) => {
+        id=result;
+    });
+    let link="http://"+req.get('host')+"/verify?id="+id;
+    let mailOptions={
+        to : req.body.email_id,
+        subject : "Please confirm your Email account",
+        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to Activate</a>" 
+    }
+    smtpTransport.sendMail(mailOptions, function(error, response){
+        if(error){
+            res.json({ success: false, msg: 'Failed to register user' + error });
+        }
+        else{
+            console.log("Message sent: " + response.message);
+            res.json({ success: true, msg: 'User Registered<br>Activation Mail sent' });
+        }
+    });
+});
+
+router.get('/verify',function(req, res, next){
+    let id = req.query.id ? req.query.id : 1;
+    if (!ObjectId.isValid(id))
+        return res.status(400).send(`NO RECORD WITH GIVEN ID : ${id}`);
+
+    var user = {
+        activated: true
+    };
+
+    User.findByIdAndUpdate(id, { $set: user }, { new: true }, (err, doc) => {
+        if (!err) {
+            res.json({ success: false, msg: 'Activation code is invalid' });
+        } else {
+            res.json({ success: true, msg: "Your Account is Activated" });
         }
     });
 });
 
 //Read Registered User
-
-
 router.get('/', function(req, res, next) {
     let page = req.query.page ? req.query.page : 1;
     User.find({ type: 'user' }).limit(config.pagination.perPage).skip(page).exec((err, docs) => {
         if (!err) {
             res.send(docs);
-        } else {
-            console.log(err);
+        } 
+        else {
+            res.send(err);
         }
-
     });
 });
-
 
 module.exports = router;
