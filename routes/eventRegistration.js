c8495fae10bd857b3a6b4aa7980cf02d1cfef065
Aravind S <saravind23399@gmail.com>
2019-01-02 23:26:00 +0530

    Tweaked Workshop Registration
    Aravind S

--- routes/eventRegistration.js
+++ routes/eventRegistration.js
@@ -5,25 +5,26 @@ const Registration = require('../models/registration');
 var nodemailer = require("nodemailer");
 var ObjectId = require('mongoose').Types.ObjectId;
 
-router.get('/checkRegistration/:event_id/:user_id', (req, res) => {
-    Registration.countDocuments({user_id: req.params.user_id, event_id: req.params.event_id}, (err, count)=>{
-        if(err){
+router.get('/checkWorkshopRegistration/:event_id/:user_id', (req, res) => {
+    Registration.countDocuments({
+        user_id: req.params.user_id,
+        event_id: req.params.event_id
+    }, (err, count) => {
+        if (err) {
             res.json({
                 error: true,
                 msg: err
             })
-        }
-        else{
-            if(count == 0 ){
+        } else {
+            if (count == 0) {
                 res.json({
-                    error:false,
+                    error: false,
                     registered: false,
                     msg: 'You can Register!'
                 })
-            }
-            else {
+            } else {
                 res.json({
-                    error:false,
+                    error: false,
                     registered: true,
                     msg: 'Already Registered!'
                 })
@@ -32,31 +33,29 @@ router.get('/checkRegistration/:event_id/:user_id', (req, res) => {
     })
 })
 
-router.post('/newWorkshopRegistration', (req, res)=>{
+router.post('/newWorkshopRegistration', (req, res) => {
     let newRegistration = undefined;
 
-    if(req.body.registration_type == 'Team'){
+    if (req.body.registration_type == 'Team') {
         newRegistration = new Registration({
             event_id: req.body.event_id,
             team_id: req.body.team_id,
             registration_type: req.body.registration_type
         })
-    }
-    else{
+    } else {
         newRegistration = new Registration({
             event_id: req.body.event_id,
             user_id: req.body.user_id,
             registration_type: req.body.registration_type
         })
     }
-    newRegistration.save((err, doc)=>{
-        if(err){
+    newRegistration.save((err, doc) => {
+        if (err) {
             res.json({
                 error: true,
                 msg: err
             })
-        }
-        else{
+        } else {
             res.json({
                 error: false,
                 msg: 'Successfully Registered!'
@@ -66,17 +65,16 @@ router.post('/newWorkshopRegistration', (req, res)=>{
 });
 
 
-router.post('/newEventRegistration', (req, res)=>{
+router.post('/newEventRegistration', (req, res) => {
     let newRegistration = undefined;
 
-    if(req.body.registration_type == 'Team'){
+    if (req.body.registration_type == 'Team') {
         newRegistration = new Registration({
             event_id: req.body.event_id,
             team_id: req.body.team_id,
             registration_type: req.body.registration_type
         })
-    }
-    else{
+    } else {
         newRegistration = new Registration({
             event_id: req.body.event_id,
             user_id: req.body.user_id,
@@ -84,14 +82,13 @@ router.post('/newEventRegistration', (req, res)=>{
         })
     }
 
-    newRegistration.save((err, doc)=>{
-        if(err){
+    newRegistration.save((err, doc) => {
+        if (err) {
             res.json({
                 error: true,
                 msg: err
             })
-        }
-        else{
+        } else {
             res.json({
                 error: false,
                 msg: 'Successfully Registered!'
