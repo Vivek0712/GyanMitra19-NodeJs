8458b3f85147e574e2757cece0b315fc5627848b
Aravind S <saravind23399@gmail.com>
2019-01-03 11:15:05 +0530

    Update eventRegistration.js
    Aravind S

--- routes/eventRegistration.js
+++ routes/eventRegistration.js
@@ -1,55 +1,76 @@
 const express = require('express');
 const router = express.Router();
-const config = require('../config/env');
 const Registration = require('../models/registration');
+const Event = require('../models/event');
 var nodemailer = require("nodemailer");
 var ObjectId = require('mongoose').Types.ObjectId;
 
 router.get('/checkWorkshopRegistration/:event_id/:user_id', (req, res) => {
-    Registration.countDocuments({
+    Registration.find({
         user_id: req.params.user_id,
         event_id: req.params.event_id
-    }, (err, count) => {
+    }, (err) => {
         if (err) {
             res.json({
                 error: true,
                 msg: err
             })
+        }
+    }).then((docs) => {
+        if (docs.length != 0) {
+            res.json({
+                error: false,
+                registered: true,
+                msg: 'Already Registered!'
+            })
         } else {
-            if (count == 0) {
-                res.json({
-                    error: false,
-                    registered: false,
-                    msg: 'You can Register!'
+            Registration.find({
+                user_id: req.params.user_id,
+                event_id: {
+                    $ne: req.params.event_id
+                }
+            }).populate('event_id').populate({
+                path: 'event_id',
+                populate: {
+                    path: 'category_id'
+                }
+            }).then((newDocs) => {
+                Event.findById(req.params.event_id).then((records) => {
+                    if (newDocs.length == 0) {
+                        res.json({
+                            error: false,
+                            registered: false,
+                            msg: 'You can Register'
+                        })
+                    } else if (records.start_time == newDocs[0].event_id.start_time) {
+                        res.json({
+                            error: false,
+                            registered: true,
+                            msg: 'Cannot Register. You have a parallel Event'
+                        })
+                    } else {
+                        res.json({
+                            error: false,
+                            registered: false,
+                            msg: 'You can Register'
+                        })
+                    }
                 })
-            } else {
-                res.json({
-                    error: false,
-                    registered: true,
-                    msg: 'Already Registered!'
-                })
-            }
+            })
         }
     })
-})
+});
 
 router.post('/newWorkshopRegistration', (req, res) => {
     let newRegistration = undefined;
-    if (req.body.registration_type == 'Team') {
-        newRegistration = new Registration({
-            event_id: req.body.event_id,
-            team_id: req.body.team_id,
-            registration_type: req.body.registration_type,
-            participation: 'Absent'
-        })
-    } else {
-        newRegistration = new Registration({
-            event_id: req.body.event_id,
-            user_id: req.body.user_id,
-            registration_type: req.body.registration_type,
-            participation: 'Absent'
-        })
-    }
+
+    newRegistration = new Registration({
+        event_id: req.body.event_id,
+        user_id: req.body.user_id,
+        registration_type: req.body.registration_type,
+        participation: 'Absent'
+    })
+
     newRegistration.save((err, doc) => {
         if (err) {
             res.json({
@@ -98,22 +119,21 @@ router.post('/newEventRegistration', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res)=>{
-    Registration.findByIdAndRemove(req.params.id, (err, docs)=>{
-        if(err){
+router.delete('/:id', (req, res) => {
+    Registration.findByIdAndRemove(req.params.id, (err, docs) => {
+        if (err) {
             res.json({
                 error: true,
                 msg: 'Unable to cancel your registration. Try again'
             })
-        }
-        else{
+        } else {
             res.json({
                 error: false,
                 msg: 'Registration removed from cart!'
             })
         }
     })
-} );
+});
 
 router.get('/:id/:type', function (req, res, next) {
     Registration.find({
@@ -135,7 +155,7 @@ router.get('/:id/:type', function (req, res, next) {
                 msg: err
             });
         } else {
-            docs = docs.filter((doc)=>{
+            docs = docs.filter((doc) => {
                 return doc.event_id.category_id.name == req.params.type
             })
             res.json(docs);
