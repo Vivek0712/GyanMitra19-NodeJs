813094dd4fd859519008da7a750643ae40e4c30e
Aravind Raj <aravinddevaraj777@gmail.com>
2019-01-23 11:58:14 +0530

    commit
    Aravind Raj M

--- index.js
+++ index.js
@@ -42,14 +42,15 @@ const payment = require('./routes/payment');
 
 //Running Port
 const port = process.env.PORT || 3000;
-var production = true;
+var production = false;
 // CORS Middleware
 
 
 if (production) {
     app.use(cors({ origin: 'http://www.gyanmitra19.mepcoeng.ac.in' }));
+    //app.use(cors({ origin: 'http://localhost:3000' }));
 } else {
-    app.use(cors({ origin: 'http://localhost:3000' }));
+    app.use(cors({ origin: 'http://localhost:4200' }));
     //port = 3000;
 }
 
